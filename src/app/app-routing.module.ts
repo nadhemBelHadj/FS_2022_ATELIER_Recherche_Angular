d17512a15d6070b99dc901a9c706a0f9585a5252
unknown <nadhemb@yahoo.com>
2022-05-04 16:28:49 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { RechercheParCategorieComponent } from './recherche-par-categorie/recherche-par-categorie.component';
+import { RechercheParNomComponent } from './recherche-par-nom/recherche-par-nom.component';
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+
+
+
+const routes: Routes = [
+  {path: "produits", component : ProduitsComponent},
+  {path: "add-produit", component : AddProduitComponent},
+  {path: "updateProduit/:id", component: UpdateProduitComponent},
+  {path: "rechercheParCategorie", component : RechercheParCategorieComponent},
+  {path: "rechercheParNom", component : RechercheParNomComponent},
+  {path: "", redirectTo: "produits", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
